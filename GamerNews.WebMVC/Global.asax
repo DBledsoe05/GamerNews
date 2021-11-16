a03c79c7194b18047861fea7d6c6320f959723fe
Denzel <dbledsoe374@gmail.com>
2021-11-15 22:44:02 -0500

    Add project files.

---
+++ GamerNews.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GamerNews.WebMVC.MvcApplication" Language="C#" %>
---
+++ GamerNews.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GamerNews.WebMVC.Startup))]
+namespace GamerNews.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
